6aea01852fe6350327c7455b9f2cabce2fd19a36
Ahmed Gabtni <agabt077@gmail.com>
2020-06-09 10:28:02 -0400

    (Proto 4) Made some css modifications on the activity

--- Proto4/js/quizzQuestion.js
+++ Proto4/js/quizzQuestion.js
@@ -51,7 +51,10 @@ class Question {
 
         for (var i = 0; i < this.choiceListElements.length; i++) {
 
-            this.choiceListElements[i].textContent = this.choices[i];
+            this.choiceListElements[i].querySelector("span").innerHTML = this.choices[i];
+            var feedbackImage = document.createElement("IMG");
+            feedbackImage.classList.add("feedback-img");
+            this.choiceListElements[i].append(feedbackImage);
             this.choiceListElements[i].parentNode.querySelectorAll("img")[0].style.transition = "all 0.5 ease-in";
 
         }
@@ -108,7 +111,10 @@ class Question {
             this.choiceListElements[c].classList.remove("selected")
             this.choiceListElements[c].classList.remove("incorrect")
             this.choiceListElements[c].classList.remove("correct")
-            this.choiceListElements[c].parentNode.querySelectorAll("img")[0].style.opacity = 0;
+
+            this.choiceListElements[c].querySelectorAll(".feedback-img")[0].style.opacity = 0;
+            this.choiceListElements[c].parentNode.querySelectorAll("#selectImage")[0].style.opacity = 0;
+
             this.feedbackElement.style.opacity = 0;
 
 
@@ -119,21 +125,21 @@ class Question {
         for (var c = 0; c < this.selectedChoices.length; c++) {
 
             this.choiceListElements[this.selectedChoices[c]].classList.add("selected");
-            this.choiceListElements[this.selectedChoices[c]].parentNode.querySelectorAll("img")[0].src = "./img/select.png";
-            this.choiceListElements[this.selectedChoices[c]].parentNode.querySelectorAll("img")[0].style.opacity = 1.0;
+            this.choiceListElements[this.selectedChoices[c]].parentNode.querySelectorAll("#selectImage")[0].src = "./img/select.png";
+            this.choiceListElements[this.selectedChoices[c]].parentNode.querySelectorAll("#selectImage")[0].style.opacity = 1.0;
         }
 
         for (var c = 0; c < this.incorrectChoices.length; c++) {
 
             this.choiceListElements[this.incorrectChoices[c]].classList.add("incorrect");
-            this.choiceListElements[this.incorrectChoices[c]].parentNode.querySelectorAll("img")[0].src = "./img/cross.png";
-            this.choiceListElements[this.incorrectChoices[c]].parentNode.querySelectorAll("img")[0].style.opacity = 1.0;
+            this.choiceListElements[this.incorrectChoices[c]].parentNode.querySelectorAll(".feedback-img")[0].src = "./img/cross.png";
+            this.choiceListElements[this.incorrectChoices[c]].parentNode.querySelectorAll(".feedback-img")[0].style.opacity = 1.0;
         }
         for (var c = 0; c < this.correctChoices.length; c++) {
 
             this.choiceListElements[this.correctChoices[c]].classList.add("correct");
-            this.choiceListElements[this.correctChoices[c]].parentNode.querySelectorAll("img")[0].src = "./img/tick.png";
-            this.choiceListElements[this.correctChoices[c]].parentNode.querySelectorAll("img")[0].style.opacity = 1.0;
+            this.choiceListElements[this.correctChoices[c]].parentNode.querySelectorAll(".feedback-img")[0].src = "./img/tick.png";
+            this.choiceListElements[this.correctChoices[c]].parentNode.querySelectorAll(".feedback-img")[0].style.opacity = 1.0;
 
 
             this.feedbackElement.style.opacity = 1.0;
